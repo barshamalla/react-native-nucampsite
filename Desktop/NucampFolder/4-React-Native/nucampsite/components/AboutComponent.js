303a67d6ed5134bbe4d0ee5bf0fdf8ea2b2f4ef0
Barsha Malla <barshamalla121@gmail.com>
2020-01-10 15:27:44 -0500

    activity indicator

--- Desktop/NucampFolder/4-React-Native/nucampsite/components/AboutComponent.js
+++ Desktop/NucampFolder/4-React-Native/nucampsite/components/AboutComponent.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 import { ScrollView, FlatList, Text } from "react-native";
-import { Card } from 'react-native-elements';
-import { PARTNERS } from '../shared/partners';
-import { ListItem } from 'react-native-elements';
+import { Card, ListItem } from 'react-native-elements';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
+
+
+const mapStateToProps = state => {
+    return {
+        partners: state.partners
+    };
+};
 
 function Mission({item}) {
     return(
@@ -26,13 +34,7 @@ function Mission({item}) {
 }
 
 class About extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            partners: PARTNERS
-        };
-    }
-    
+
     static navigationOptions = {
         title: 'About Us'
     };
@@ -43,17 +45,40 @@ class About extends Component {
                 <ListItem
                     title={item.name}
                     subtitle={item.description}
-                    //onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })}
-                    leftAvatar={{ source: require('./images/bootstrap-logo.png')}}
+                    leftAvatar={{source: {uri: baseUrl + item.image}}}
                 />
             );
         };
+        if (this.props.partners.isLoading) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card
+                        title='Community Partners'>
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            );
+        }
+        if (this.props.partners.errMess) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card
+                        title='Community Partners'>
+                        <Text>{this.props.partners.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            );
+        }
+        
+
         return(
                 <ScrollView>
                     <Mission/>
                     <Card title="Community Partners">
                     <FlatList
-                data={this.state.partners}
+                data={this.props.partners.partners}
                 renderItem={renderPartner}
                 keyExtractor={item => item.id.toString()}
             />
@@ -64,4 +89,4 @@ class About extends Component {
 }
 
 
-export default About;
+export default connect(mapStateToProps)(About);
--- Desktop/NucampFolder/4-React-Native/nucampsite/components/ContactComponent.js
+++ Desktop/NucampFolder/4-React-Native/nucampsite/components/ContactComponent.js
@@ -15,10 +15,9 @@ class Contact extends Component {
                     <Card
                     title="Contact Information" 
                     wrapperStyle={{margin: 20}}>
-                        <Text  style={{marginBottom: 10}} > 
-                            1 Nucamp Way {'\n'}
-                            Seattle, WA 98001 {'\n'}
-                            U.S.A.</Text>
+                        <Text>1 Nucamp Way </Text>
+                        <Text>Seattle, WA 98001 </Text>
+                        <Text style={{marginBottom: 10}}>U.S.A.</Text>
                         <Text>Phone: 1-206-555-1234</Text>
                         <Text>Email:campsites@nucamp.co</Text>
                     </Card>
