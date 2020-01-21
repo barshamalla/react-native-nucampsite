e7a8d23e26024a1dde6ac80b1dce4862f32c8bf4
Barsha Malla <barshamalla121@gmail.com>
2020-01-21 13:52:47 -0500

    Exercise:Local Notification

--- Desktop/NucampFolder/4-React-Native/nucampsite/components/ReservationComponent.js
+++ Desktop/NucampFolder/4-React-Native/nucampsite/components/ReservationComponent.js
@@ -4,6 +4,8 @@ import { Text, View, StyleSheet,
 import DatePicker from 'react-native-datepicker';
 import * as Animatable from 'react-native-animatable';
 import { Card } from 'react-native-elements';
+import * as Permissions from 'expo-permissions';
+import { Notifications } from 'expo';
 
 class Reservation extends Component {
 
@@ -38,7 +40,10 @@ class Reservation extends Component {
                 },
                 {
                     text: 'OK',
-                    onPress: () => this.resetForm()
+                    onPress: () => {
+                        this.presentLocalNotification(this.state.date);
+                        this.resetForm();
+                    }
                 }
             ],
             { cancelable: false }
@@ -55,11 +60,33 @@ class Reservation extends Component {
         });
        
     }
+
+    async obtainNotificationPermission() {
+        const permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+        if (permission.status !== 'granted') {
+            const permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+            if (permission.status !== 'granted') {
+                Alert.alert('Permission not granted to show notifications');
+            }
+            return permission;
+        }
+        return permission;
+    }
+
+    async presentLocalNotification(date) {
+        const permission = await this.obtainNotificationPermission();
+        if (permission.status === 'granted') {
+            Notifications.presentLocalNotificationAsync({
+                title: 'Your Campsite Reservation Search',
+                body: 'Search for ' + date + ' requested'
+            });
+        }
+    }
       
     render() {
         return (      
             <Animatable.View
-            animation='zoomInUp' 
+            animation='zoomIn' 
             duration={2000} 
             delay={1000}
              >
